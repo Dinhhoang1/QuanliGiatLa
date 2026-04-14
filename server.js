ac5e9a86edc1df0d716f87620bd29707dd883a1d
dinhhoang <dinhhoangxz.2402@gmail.com>
2026-04-15 01:42:51 +0700

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const pool = mysql.createPool({
 
     password: 'AVNS_O3UPYLpAtHgt3GqXJ0D', // Điền mật khẩu Aiven
 
-    database: 'QuanliTiemGat', 
+    database: 'QuanliTiemGiat', 
 
     port: 13501, // Điền Port Aiven của bạn
 
